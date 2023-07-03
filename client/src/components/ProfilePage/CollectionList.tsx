66955be464b9293a87af084ff43e5691c0e7903e
sprite-enjoyer <samkharadze.saba01@gmail.com>
2023-07-03 17:51:25 +0400

    vercel build fix

--- client/src/components/ProfilePage/CollectionList.tsx
+++ client/src/components/ProfilePage/CollectionList.tsx
@@ -1,21 +1,26 @@
 import { observer } from "mobx-react";
 import ProfilePageStore from "../../stores/ProfilePageStore";
 import CollectionCard from "./CollectionCard";
+import { ReactElement } from "react";
 
 export interface CollectionListProps {
   profilePageStore: ProfilePageStore;
 }
 
 const CollectionList = ({ profilePageStore }: CollectionListProps) => {
-  return profilePageStore.collections.map((col, i) => (
-    <CollectionCard
-      key={i}
-      name={col.name}
-      image={col.image}
-      description={col.description}
-      id={col._id}
-    />
-  ));
+  return (
+    <>
+      {profilePageStore.collections.map((col, i) => (
+        <CollectionCard
+          key={i}
+          name={col.name}
+          image={col.image}
+          description={col.description}
+          id={col._id}
+        />
+      ))}
+    </>
+  );
 };
 
 export default observer(CollectionList);
